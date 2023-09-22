8d8aeb7310c085c7e0ae70eba5d507f033042e44
Lucas Ivisson <lucasivisson1@gmail.com>
2023-09-22 12:29:50 -0300

    🚧 working with stylesheet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { Text, View } from 'react-native'
-import { Fragment } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#131016', padding: 24 }}>
-      <Text style={{ color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48 }}>Nome do evento</Text>
-      <Text style={{ color: '#FDFCFE', fontSize: 16 }}>Sexta, 4 de Novembro de 2022.</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
     </View >
   )
 }
+
+const styles = StyleSheet.create({
+  container: {      
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: { color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48 },
+  eventDate: { color: '#FDFCFE', fontSize: 16 }
+})

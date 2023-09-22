30d142fdac5a8561d4297ed9e70f4a89b5f5cb79
Lucas Ivisson <lucasivisson1@gmail.com>
2023-09-22 11:20:53 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+import { Fragment } from 'react'
+
+export default function App() {
+  return (
+    <Fragment>
+      <Text>React Native</Text>
+      <Text>React Native</Text>
+    </Fragment>
+  )
+}

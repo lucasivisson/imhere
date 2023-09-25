3d4daf030bd1c80aee729e6a0bde7dd454643782
Lucas Ivisson <lucasivisson1@gmail.com>
2023-09-25 16:08:07 -0300

    ✨ introduce remove participant function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -29,7 +29,10 @@ export function Home() {
     Alert.alert("Remover", `Deseja remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!"),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
